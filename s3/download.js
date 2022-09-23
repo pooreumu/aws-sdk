95e036f7bfdf40eaf695eeae99954a04e5fd9401
pooreumu <pooreum83@gmail.com>
2022-09-23 11:35:28 +0900

    deprecated된 거 수정
    Changes to be committed:
    	modified:   s3/.env.example
    	modified:   s3/download.js
    	new file:   s3/test1.json
    	modified:   s3/upload.js

--- s3/download.js
+++ s3/download.js
@@ -1,16 +1,14 @@
 require("dotenv").config();
 const fs = require("fs");
 const AWS = require("aws-sdk");
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const s3 = new AWS.S3({ accessKeyId, secretAccessKey });
+const s3 = new AWS.S3({ region: process.env.AWS_REGION });
 const downloadFile = (params) => {
     s3.getObject(params, (error, data) => {
         if (error) console.log(error);
         const result = data.Body.toString();
         const parse = JSON.parse(result);
         console.log(parse);
-        fs.writeFileSync(fileName, data);
+        fs.writeFileSync(params.Key, result);
     });
 };
 downloadFile({
--- s3/upload.js
+++ s3/upload.js
@@ -1,13 +1,9 @@
 require("dotenv").config();
 const fs = require("fs");
 const AWS = require("aws-sdk");
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const s3 = new AWS.S3({ accessKeyId, secretAccessKey });
+const s3 = new AWS.S3({ region: process.env.AWS_REGION });
 
-const uploadFile = (fileName) => {
-    const bucket = "버켓이름";
-    const key = "객체이름";
+const uploadFile = ({ bucket, key, fileName }) => {
     const fileContent = fs.readFileSync(fileName);
     const params = {
         Bucket: bucket,
@@ -19,4 +15,8 @@ const uploadFile = (fileName) => {
         console.log(`성공 ${data.Location}`);
     });
 };
-uploadFile("./anyfile.json");
+uploadFile({
+    bucket: "버켓이름",
+    key: "객체이름",
+    fileName: "./test1.json",
+});
